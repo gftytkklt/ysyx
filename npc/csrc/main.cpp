26347f60bc90147a7a86b43e1ad4a4133027e782
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 17:01:24 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:01:24 up  1:47,  1 user,  load average: 0.11, 0.08, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -2,23 +2,40 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "verilated.h"
+#include "verilated_vcd.h"
 #include "Vexample.h"
 vluint64_t sim_time = 0;
 int main(int argc, char** argv) {
   printf("Hello, ysyx!\n");
+  // declear global configuration
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  // waveform file pointer
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  // parameters(do not really understand)
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
+  // top module defination
   Vexample* switches = new Vexample{contextp};
+  // input initialization
+  switches->a = 0;
+  switches->b = 0;
+  // waveform gen
+  switches->trace(tfp,0);
+  tfp->open("switch_sim.vcd");
+  // set stop conditions manually by sim time
   while (!contextp->gotFinish() && sim_time < 50){
 	  int a = rand() & 1;
 	  int b = rand() & 1;
 	  switches->a = a;
 	  switches->b = b;
 	  switches->eval();
+	  tfp->dump(sim_time);
 	  printf("a=%d, b=%d, z=%d\n", a, b, switches->z);
 	  assert(switches->z == (a^b));
 	  sim_time++;
   }
+  tfp->close();
   delete switches;
   delete contextp;
   return 0;
