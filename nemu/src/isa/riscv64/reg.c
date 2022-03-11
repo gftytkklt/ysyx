b24b31e4bbd196d8125b0d8c810725b0c8b19f98
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 19:43:29 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:43:29 up 57 min,  1 user,  load average: 0.04, 0.11, 0.08

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -5,12 +5,12 @@ const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t60"
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
 void isa_reg_display() {
   for (int i=0;i<32;i++){
-    printf("%ld\n",sizeof(regs));
+    printf("%ld %ld\n",sizeof(regs),sizeof(char*));
     printf("%s: %lx\n", regs[i], cpu.gpr[i]);
   }
 }
