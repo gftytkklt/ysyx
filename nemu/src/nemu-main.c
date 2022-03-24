c64485070f2be086768699ea1c08beaa7d7ccd55
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 17:02:17 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:17 up  1:37,  1 user,  load average: 0.21, 0.14, 0.10

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
   //return is_exit_status_bad();
   // modified for test expr.c
   FILE *fp1;
-  fp1 = fopen("/home/gftyt/ysyx-workbench/nemu/src/input.txt","r");
+  fp1 = fopen("/home/gftyt/ysyx-workbench/nemu/src/input","r");
   if (fp1==NULL){puts("fopen error\n");return 0;}
   char line[1024] = {}; 
   while ((fgets (line, 1024, fp1)) != NULL)
