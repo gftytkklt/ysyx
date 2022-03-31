4aad9ff8c0947c08bba64ac29d24a560252e3a35
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 16:59:41 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:59:41 up 33 min,  1 user,  load average: 0.10, 0.03, 0.01

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -12,6 +12,7 @@ typedef struct watchpoint {
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
+//static int wp_num = 0;
 
 void init_wp_pool() {
   int i;
@@ -20,24 +21,48 @@ void init_wp_pool() {
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
 
-  head = NULL;
-  free_ = wp_pool;
+  // head = NULL;
+  // free_ = wp_pool;
+  // use virtual head node
+  head->NO = -1;
+  head->next = NULL;
+  free_->NO = -1;
+  free_->next = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
-
+// occupied node from 0-N sequentially
+// fetch head of free WP list, insert it to head of occupied WP list
 WP* new_wp() {
-  if (free_==NULL) {
+  WP *new_node;
+  if (free_->next == NULL) {
     printf("no more watchpoints!\n");
     return NULL;
   }
   else {
-    head = free_;
-    free_ = free_->next;
-    return head;
+    new_node = free_->next;
+    free_->next = free_->next->next;
+    new_node->next = head->next;
+    head->next = new_node;
   }
+  //new_node = free_;
+  //free_ = free_->next;
+  //new_node->next = NULL;
+  //wp_num ++;
+  return new_node;
 }
 
+// add new free WP to head of free WP list
 void free_wp(WP* wp) {
-  //if (head)
+  if (head->next == NULL){printf("no used watchpoint!\n");return;}
+  for (WP* wp_pt = head;wp_pt->next != NULL;wp_pt = wp_pt->next) {
+    if (wp_pt->next == wp) {
+      wp_pt->next = wp_pt->next->next;
+      wp->next = free_->next;
+      free_->next = wp;
+      return;
+    }
+  }
+  printf("NO.%d WP not found!\n", wp->NO);
+  return;
 }
