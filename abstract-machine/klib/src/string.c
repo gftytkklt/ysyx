3b44cc16097c224ea6d8e5647333ee90a35024f5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 23:11:44 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:11:43 up  1:39,  1 user,  load average: 0.03, 0.10, 0.05

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,27 +9,65 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i = 0;
+  while(src[i] != '\0'){
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i = 0;
+  while((src[i] != '\0') && (i<n)){
+    dst[i] = src[i];
+    i++;
+  }
+  if(i<n){dst[i] = '\0';}
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i=0;
+  while(dst[i] != '\0') {
+    i++;
+  }
+  for (size_t j=0;src[j] != '\0';j++) {
+    dst[i] = src[j];
+    i++;
+  }
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i=0;
+  while((s1[i]!='\0') || (s2[i]!='\0')) {
+    if (s1[i] != s2[i]) {return (s1[i] - s2[i]);}
+    i++;
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  for(size_t i=0;i<n;i++) {
+    if (s1[i] != s2[i]) {return (s1[i] - s2[i]);}
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  unsigned char *tmp = s;
+  for (size_t i=0;i<n;i++) {
+    tmp[i] = (unsigned char) c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +79,14 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  if(n==0){return 0;}
+  const unsigned char* tmp1 = s1;
+  const unsigned char* tmp2 = s2;
+  for (size_t i=0;i<n;i++){
+    if (tmp1[i] != tmp2[i]) {return (tmp1[i] - tmp2[i]);}
+  }
+  return 0;
 }
 
 #endif
