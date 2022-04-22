c3aeda7eb58e2a6f36fdf1a482b0cab1b5886296
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-22 12:28:04 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:28:04 up  2:26,  1 user,  load average: 0.22, 0.06, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,54 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *tmp = out;
+  va_list ap;
+  va_start(ap, fmt);
+  for (size_t i=0;fmt[i]!='\0';i++){
+    if(fmt[i] == '%'){
+      if(fmt[i+1] == 'd'){
+        int val = va_arg(ap, int);
+        if(val == 0){
+          *tmp = (unsigned char) 48;
+          tmp++;
+        }
+        else {
+          if(val<0){val = -val;*tmp = '-';tmp++;}
+          int bit = 0;
+          while(val!=0){
+            bit = val % 10;
+            *tmp = (unsigned char) (bit+48);
+            tmp++;
+            val = val / 10;
+          }
+        }
+      }
+      else if(fmt[i+1] == 's'){
+        char *str = va_arg(ap, char*);
+        while(*str != '\0'){
+          *tmp = *str;
+          tmp++;str++;
+        }
+      }
+      else if(fmt[i+1] == '%'){
+        *tmp = fmt[i];
+        tmp++;
+      }
+      else {
+      	printf("undefined sign \%\n");
+      	continue;
+      }
+      i++;
+    }
+    else {
+      *tmp = fmt[i];
+      tmp++;
+    }
+  }
+  va_end(ap);
+  *tmp = '\0';
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
