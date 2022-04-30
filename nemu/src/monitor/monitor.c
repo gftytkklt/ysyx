e5a50c00b67ff41799616ce2863d14bc59ff0c37
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-30 19:50:53 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:50:53 up  7:28,  1 user,  load average: 0.07, 0.12, 0.06

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -215,18 +215,18 @@ void print_ftrace(unsigned long pc, unsigned long dnpc, unsigned inst) {
   for(int i=0;i<func_idx;i++){  
     //printf("%s %lx\n", func_pool[func_idx].func_name, func_pool[func_idx].entry_addr);
     if(dnpc == func_pool[i].entry_addr){
-      printf("%lx:%*s",pc,func_depth*2," ");
+      printf("%lx:%*s",pc,func_depth," ");
       // print info & depth update
       printf("call [%s@0x%lx]\n",func_pool[i].func_name,func_pool[i].entry_addr);func_depth++;
     }
     else if((dnpc > func_pool[i].entry_addr) && (dnpc < func_pool[i].entry_addr + func_pool[i].func_size) && (inst==0x00008067)){
       func_depth--;
-      printf("%lx:%*s",pc,func_depth*2," ");
+      printf("%lx:%*s",pc,func_depth," ");
       printf("ret [%s]\n",func_pool[i].func_name);
     }
     else if(inst == 0x00100073){
       func_depth--;
-      printf("%lx:%*sebreak\n",pc,func_depth*2," ");
+      printf("%lx:%*sebreak\n",pc,func_depth," ");
       break;
     }
   }
