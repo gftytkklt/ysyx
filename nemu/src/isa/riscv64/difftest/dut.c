c8fa41d9b35c1b724e6cba677472dd806e0dc380
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 18:49:01 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:49:01 up 0 min,  1 user,  load average: 0.89, 0.28, 0.10

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -6,7 +6,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool a;
   bool b = true;
   for (int i=0;i<32;i++){
-    a = difftest_check_reg(reg_name(i,0), pc, ref_r->gpr[i], cpu.gpr[i]+1);
+    a = difftest_check_reg(reg_name(i,0), pc, ref_r->gpr[i], cpu.gpr[i]);
     if(!a){b=false;}
   }
   return b;
