84f1b2893de3bd77aafc51fcd2bd8678bb43780f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 15:05:49 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:05:49 up  1:15,  1 user,  load average: 0.02, 0.03, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -69,11 +69,70 @@ int sprintf(char *out, const char *fmt, ...) {
   }
   va_end(ap);
   *tmp = '\0';
-  return 0;
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *tmp = out;
+  va_list ap;
+  va_start(ap, fmt);
+  for (size_t i=0;fmt[i]!='\0';i++){
+    if(tmp == &out[n-1]){
+      *tmp = '\0';
+      return strlen(fmt);
+    }
+    if(fmt[i] == '%'){
+      if(fmt[i+1] == 'd'){
+        int val = va_arg(ap, int);
+        if(val == 0){
+          *tmp = (unsigned char) 48;
+          tmp++;
+        }
+        else {
+          char a[20] = "\0";// int have 10 bits at most
+          if(val<0){val = -val;*tmp = '-';tmp++;}// -2^31 will overflow
+          int bit = 0;
+          int cnt = 0;// a index
+          while(val!=0){
+            bit = val % 10;
+            //*tmp = (unsigned char) (bit+48);
+            //tmp++;
+            a [cnt] = (unsigned char) (bit+48);
+            cnt++;
+            val = val / 10;
+          }
+          for(int cnt_rev=cnt-1;cnt_rev>=0;cnt_rev--){
+            *tmp = a[cnt_rev];
+            tmp++;
+          }
+        }
+      }
+      else if(fmt[i+1] == 's'){
+        char *str = va_arg(ap, char*);
+        while(*str != '\0'){
+          *tmp = *str;
+          tmp++;str++;
+        }
+      }
+      else if(fmt[i+1] == '%'){
+        *tmp = fmt[i];
+        tmp++;
+      }
+      else {
+      	printf("undefined sign \%\n");
+      	continue;
+      }
+      i++;
+    }
+    else {
+      *tmp = fmt[i];
+      tmp++;
+    }
+  }
+  va_end(ap);
+  *tmp = '\0';
+  return strlen(out);
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
