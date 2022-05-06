c854f6eab35f871ce5259c6833eb59a2622779e4
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 23:37:56 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:37:56 up  3:21,  1 user,  load average: 0.01, 0.03, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,19 +1,19 @@
 #include <am.h>
 #include <nemu.h>
-//static uint64_t boot_time = 0;
+static uint64_t boot_time = 0;
 //uint64_t get_time();
 void __am_timer_init() {
   //void *rtc_addr = RTC_ADDR;
   //printf("boot time %d\n", boot_time);
   //ioe_write(AM_TIMER_UPTIME, rtc_addr);
-  //ioe_read(AM_TIMER_UPTIME, &boot_time);
+  ioe_read(AM_TIMER_UPTIME, &boot_time);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //void *rtc_addr = RTC_ADDR;
   //ioe_write(AM_TIMER_UPTIME, rtc_addr);
   //uptime->us -= boot_time;
-  uptime->us = 0;
+  uptime->us = boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
