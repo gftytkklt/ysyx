85f4c3a1f35221a12b7117f63df047b934e371d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 00:16:58 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:16:58 up  4:09,  1 user,  load average: 0.12, 0.19, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,9 +6,9 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //kbd->keydown = 1;
   //kbd->keydown = (kbd->keycode == AM_KEY_NONE) ? 0 : 1;
-  int x = inl(KBD_ADDR);
+  //int x = inl(KBD_ADDR);
   //putch((char)x);
   //printf("%x\n",KBD_ADDR);
-  kbd->keycode = x;
+  //kbd->keycode = x;
   //kbd->keydown = (kbd->keycode == AM_KEY_NONE) ? 0 : 1;
 }
