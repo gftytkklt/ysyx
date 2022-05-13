3b96900ca545408ec9ee731b49df226518c13aa9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 16:33:52 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:52 up  1:41,  1 user,  load average: 0.01, 0.04, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,11 +2,12 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+static int w = 0;
+static int h = 0;
 void __am_gpu_init() {
   int i;
-  int w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
-  int h = inw(VGACTL_ADDR);  // TODO: get the correct height
+  w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
+  h = inw(VGACTL_ADDR);  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -21,8 +22,28 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  // x, y: current block top left index
+  // *pixels: pixel data of the block
+  // format: row1-row2-...-rown
+  // w, h: size of drawing block
+  // sync: finish drawing
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  else {
+    int x = ctl->x;
+    int y = ctl->y;
+    int *pixels = ctl->pixels;
+    int blk_w = ctl->w;
+    int blk_h = ctl->h;
+    int base = w*y+x;
+    for (int row=0;row<blk_h;row++){
+      for(int col=0;col<blk_w;col++){
+        int index = row*blk_w+col;
+        int offset = row*w+col;
+        outl(FB_ADDR+base+offset,pixels[index]);
+      }
+    }
   }
 }
 
