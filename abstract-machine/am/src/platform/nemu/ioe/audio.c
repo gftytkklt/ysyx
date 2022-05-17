2cf7dc7fda89cc5604db2e78efebb5ecfc170417
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 21:55:33 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:55:32 up  2:32,  1 user,  load average: 0.30, 0.13, 0.04

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -7,20 +7,39 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+static int bufsize = 0;
 
 void __am_audio_init() {
+  bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  outl(AUDIO_INIT_ADDR,1);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;// modify it to false to disable audio
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);// 64K
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  // TODO();
+  // outl something
+  int len = ctl->buf.end-ctl->buf.start;
+  char *play_buf = (char *)ctl->buf.start;
+  // wait until enough buf len for write
+  while(bufsize - inl(AUDIO_COUNT_ADDR) < len);
+  // write audio to buf
+  for (int i=0;i<len;i++) {
+    outb(AUDIO_SBUF_ADDR+inl(AUDIO_COUNT_ADDR),play_buf[i]);
+  }
+  // update count
+  outl(inl(AUDIO_COUNT_ADDR)+len,AUDIO_COUNT_ADDR);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -11,11 +11,56 @@ enum {
   reg_count,
   nr_reg
 };
-
+static volatile int count = 0;
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+SDL_AudioSpec s = {};
+static uint32_t audio_freq() {
+  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_AUDIO_CTRL).freq, audio_base[0]);
+}
+
+static uint32_t audio_channels() {
+  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_AUDIO_CTRL).channels, audio_base[1]);
+}
+
+static uint32_t audio_samples() {
+  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_AUDIO_CTRL).samples, audio_base[2]);
+}
+
+static void audio_play(void *userdata, uint8_t *stream, int len){
+  int nread = len;
+  if (count < len) nread = count;
+  //int b = 0;
+  //while (b < nread) {
+  //  int n = read(rfd, stream, nread);
+  //  if (n > 0) b += n;
+  //}
+
+  count -= nread;
+  if (len > nread) {
+    memset(stream + nread, 0, len - nread);
+  }
+}
+
+static void SDL_audio_init(){
+  SDL_AudioSpec s = {};
+  s.freq = audio_freq();
+  s.format = AUDIO_S16SYS;
+  s.channels = audio_channels();
+  s.samples = audio_samples();
+  s.callback = audio_play;
+  s.userdata = NULL;
+
+  count = 0;
+  int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+  if (ret == 0) {
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  }
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  
 }
 
 void init_audio() {
@@ -29,4 +74,7 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  while(!audio_base[4]);// wait until __am_audio_init done
+  SDL_audio_init();
+  //memset(audio_base, 0, CONFIG_SB_SIZE);
 }
