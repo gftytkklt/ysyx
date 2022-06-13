b7835051c9f32308f2755c1ddcabd467e66e8ac6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 23:33:03 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:33:03 up 40 min,  1 user,  load average: 0.02, 0.05, 0.12

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -137,7 +137,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 111 ????? 11100 11", csrrci   , IS, R(dest) = C(src2); C(src2) &= ~src1);
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall , N, s->dnpc = isa_raise_intr(isa_query_intr(), s->pc));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , N, s->dnpc = C(MEPC));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , N, s->pc = C(MEPC));
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
