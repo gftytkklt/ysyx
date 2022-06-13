33fdac1208a084d59de5d7c46d63e46f14d566d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 23:31:35 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:31:35 up 39 min,  1 user,  load average: 0.01, 0.06, 0.13

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -4,10 +4,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  cpu.csr[0] = epc;
-  cpu.csr[2] = NO;
+  cpu.csr[0] = epc;// mepc = epc
+  cpu.csr[2] = NO;// mcause = NO
   
-  return cpu.csr[3];
+  return cpu.csr[3];// dnpc = mtvec
 }
 
 word_t isa_query_intr() {
