f358e92218e71fbaf28a04534d4ad7ce2bda3489
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 20:20:35 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:20:35 up  2:02,  1 user,  load average: 0.05, 0.08, 0.05

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("cause: %p\n", c->mcause);
+    printf("cause: %x\n", c->mcause);
     switch (c->mcause) {
       
       default: ev.event = EVENT_ERROR; break;
