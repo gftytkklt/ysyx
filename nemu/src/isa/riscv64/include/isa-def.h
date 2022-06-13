6bbb77b64e903eedfa0abd07409f54cd2889861b
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 21:40:54 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:54 up  3:23,  1 user,  load average: 0.30, 0.17, 0.10

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -6,8 +6,8 @@
 typedef struct {
   word_t gpr[32];
   //word_t mepc, mstatus, mcause, mtvec;
-  word_t csr[4]; //mepc, mstatus, mcause, mtvec;
   vaddr_t pc;
+  word_t csr[4]; //mepc, mstatus, mcause, mtvec;
 } riscv64_CPU_state;
 
 // decode
