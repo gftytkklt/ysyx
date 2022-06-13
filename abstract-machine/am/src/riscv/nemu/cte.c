e3669ffc93a73e2714a0b0feef4fcaf4d4f0a664
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-13 23:51:38 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:51:37 up 59 min,  1 user,  load average: 0.16, 0.11, 0.10

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,8 +14,8 @@ Context* __am_irq_handle(Context *c) {
     }
     //printf("cause: %ld\n", c->mcause);
     switch (c->mcause) {
-      
-      default: ev.event = EVENT_YIELD; break;
+      case 0x0b: ev.event = EVENT_YIELD; break;
+      default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
