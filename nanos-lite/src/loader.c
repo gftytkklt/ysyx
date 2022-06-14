7baabb63471716f8bee243932377888ea9b3b741
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 00:29:27 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:29:27 up 19 min,  1 user,  load average: 0.01, 0.08, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,9 +8,13 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  Elf_Ehdr ehdr  = {};
+  ramdisk_read(&ehdr, 0, 64);
+  assert(*(uint32_t *)&ehdr.e_ident == 0x464c457f);
+  //TODO();
   return 0;
     
 }
