0a9c68afed3c04e94f8aa3bf07d9a2fb8b54eddd
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-14 15:53:38 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:38 up  2:17,  1 user,  load average: 0.31, 0.16, 0.06

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -6,10 +6,16 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.csr[0] = epc;// mepc = epc
   cpu.csr[2] = NO;// mcause = NO
-  
   return cpu.csr[3];// dnpc = mtvec
+  //assert((BITS(cpu.csr[3],1,0) == 2) || (BITS(cpu.csr[3],1,0) == 0);
+  //return (BITS(cpu.csr[3],1,0) == 0) ? cpu.csr[3] : ((cpu.csr[3] & 0xfffffffffffffffc) + NO*4);// dnpc = mtvec
 }
 
 word_t isa_query_intr() {
-  return INTR_EMPTY;
+  switch(cpu.gpr[17]){
+    case -1: return INTR_EMPTY & 0x0b;
+    default: return INTR_EMPTY;
+  }
+  
+  //return INTR_EMPTY;
 }
