df5f954384de40e3f2ca19269c51347e5f840f3c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 12:22:15 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:22:15 up 43 min,  1 user,  load average: 0.11, 0.21, 0.25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,12 +5,16 @@ static int sys_yield(){
   yield();
   return 0;
 }
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
 
+void sys_exit(uintptr_t ret){
+  halt(ret);
+}
+void do_syscall(Context *c) {
+  uintptr_t a[4], ret;
+  a[0] = c->GPR1;
+  ret = c->GPRx;
   switch (a[0]) {
-    //case SYS_exit: TODO();break;
+    case SYS_exit: sys_exit(ret);break;
     case SYS_yield: sys_yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
