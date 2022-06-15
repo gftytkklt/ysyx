f9e9c615974ced7662778557fd27a19017b4a384
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 12:27:28 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:27:28 up 48 min,  1 user,  load average: 0.13, 0.16, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,9 @@ void sys_exit(uintptr_t ret){
 void do_syscall(Context *c) {
   uintptr_t a[4], ret;
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   ret = c->GPRx;
   switch (a[0]) {
     case SYS_exit: sys_exit(ret);break;
