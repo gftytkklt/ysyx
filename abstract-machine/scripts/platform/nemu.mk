48f7503282784673f1c8b66830f0cc2ff3bb0116
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 18:47:50 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:47:50 up  1:55,  1 user,  load average: 0.08, 0.04, 0.05

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,9 +13,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += -b
-#ifdef CONFIG_FTRACE
-NEMUFLAGS += -e $(IMAGE).elf
-#endif
+NEMUFLAGS += $(if CONFIG_FTRACE, -e $(IMAGE).elf, )
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
 .PHONY: $(AM_HOME)/am/src/platform/nemu/trm.c
