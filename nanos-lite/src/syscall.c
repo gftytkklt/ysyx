4b5aade4bd851f6cb8b5a50c243dbeffc874d4c3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 12:49:07 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:49:06 up  1:07,  1 user,  load average: 0.11, 0.16, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,8 +1,10 @@
 #include <common.h>
 #include "syscall.h"
+#ifdef CONFIG_STRACE
 void print_strace(Context *c){
   printf("type: %ld, parameters: %ld, %ld, %ld, ret: %ld\n",c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
 }
+#endif
 static int sys_yield(){
   yield();
   return 0;
@@ -18,7 +20,9 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   ret = c->GPRx;
+  #ifdef CONFIG_STRACE
   print_strace(c);
+  #endif
   switch (a[0]) {
     case SYS_exit: sys_exit(ret);break;
     case SYS_yield: sys_yield();break;
