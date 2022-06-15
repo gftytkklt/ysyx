6132a6a7e369cd82024ba430f009d9ae38f68188
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 18:27:54 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:27:54 up  1:35,  1 user,  load average: 0.10, 0.09, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,8 +2,13 @@
 #include "syscall.h"
 #define CONFIG_STRACE
 #ifdef CONFIG_STRACE
-void print_strace(Context *c){
-  printf("type: %ld, parameters: %ld, %ld, %ld, ret: %ld\n",c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+void print_strace(uintptr_t a1,uintptr_t a2,uintptr_t a3,uintptr_t a4,uintptr_t ret, bool has_ret){
+  if(has_ret){
+    printf("type: %ld, parameters: %ld, %ld, %ld, ret: %ld\n",a1,a2,a3,a4,ret);
+  }
+  else{
+    printf("type: %ld, parameters: %ld, %ld, %ld\n",a1,a2,a3,a4);
+  }
 }
 #endif
 //extern char end;
@@ -39,20 +44,22 @@ int brk(void *addr){
   return 0;
 }
 void do_syscall(Context *c) {
-  uintptr_t a[4];
+  uintptr_t a[4], ret;
+  bool has_ret = true;
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   //ret = c->GPRx;
   switch (a[0]) {
-    case SYS_exit: sys_exit(a[1]);break;
+    case SYS_exit: sys_exit(a[1]);has_ret=false;break;
     case SYS_yield: c->GPRx = sys_yield();break;
     case SYS_write: c->GPRx = write((int)a[1],(void*)a[2],(size_t)a[3]);break;
     case SYS_brk: c->GPRx = brk((void*)a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  ret = c->GPRx;
   #ifdef CONFIG_STRACE
-  print_strace(c);
+  print_strace(a[0],a[1],a[2],a[3],ret,has_ret);
   #endif
 }
