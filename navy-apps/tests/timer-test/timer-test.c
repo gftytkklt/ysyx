78f179dc5f2d4dc369c47e52e48ddff5ae732c51
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-17 20:20:47 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:20:46 up 21 min,  1 user,  load average: 0.59, 0.36, 0.31

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,17 +1,17 @@
-#include <time.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 
 static struct timeval boot_time = {};
 
 int main() {
-  long half_sec = 1;
-  gettimeofday(&boot_time, NULL);
-  struct timeval cur_time = {};
+  NDL_Init(0);
+  uint32_t half_sec = 1;
+  //gettimeofday(&boot_time, NULL);
+  //struct timeval cur_time = {};
   while(1){
-    gettimeofday(&cur_time, NULL);
-    if((cur_time.tv_usec-boot_time.tv_usec) / 500000 < half_sec){continue;}
-    printf("%ld half sec(s)\n", half_sec);
+    //gettimeofday(&cur_time, NULL);
+    while(NDL_GetTicks() / 500 < half_sec);
+    printf("%d half sec(s)\n", half_sec);
     half_sec++;
   }
   return 0;
