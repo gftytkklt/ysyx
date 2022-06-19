b8dac4fc44f9655c48a55e71c005e449e7f3053d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-20 01:51:42 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:51:42 up 5 min,  1 user,  load average: 0.39, 0.94, 0.55

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -81,9 +81,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_FBDRAW_T fbdraw = {.y = offset/cfg.width,
                             .x = offset%cfg.width,
                             .pixels = (void*)buf,
-                            .w=len>>32,
-                            .h=len&0xffffffff};
-  //printf("%d %d %d %d %ld %ld\n",fbdraw.x, fbdraw.y, fbdraw.w, fbdraw.h, offset, len);
+                            .w=(len>>32),
+                            .h=(len&0xffffffff)};
+  printf("%d %d %d %d %ld %ld\n",fbdraw.x, fbdraw.y, fbdraw.w, fbdraw.h, offset, len);
   io_write(AM_GPU_FBDRAW, fbdraw.x, fbdraw.y, fbdraw.pixels, fbdraw.w, fbdraw.h, 1);
   return fbdraw.w*fbdraw.h*4;
 }
