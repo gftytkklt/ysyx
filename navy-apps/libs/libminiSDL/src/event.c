eb123a8db95a2acebc8128c6dabba9eb5298818a
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 19:49:00 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:00 up  5:10,  1 user,  load average: 0.26, 0.42, 0.33

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -13,7 +13,21 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char buf[64];
+  if (!NDL_PollEvent(buf, 64)){return 0;}
+  else {
+    char key_status[5];
+    char key_name[20];
+    int name_id=0;
+    sscanf(buf,"%s %s",key_status, key_name);
+    if(!strcmp(key_status, "kd")){ev->type = SDL_KEYDOWN;}
+    else{ev->type = SDL_KEYUP;}
+    while(name_id < sizeof(keyname)/sizeof(const char*)){
+      if(!strcmp(key_name, keyname[name_id])){ev->key.keysym.sym = name_id;break;}
+      name_id++;
+    }
+  }
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
