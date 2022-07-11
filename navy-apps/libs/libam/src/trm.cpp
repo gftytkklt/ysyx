dc4711ed7e5c6ccc28a1e9472dbdc157c4fc02fe
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 13:20:19 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:20:19 up 10 min,  1 user,  load average: 0.21, 0.18, 0.11

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -5,8 +5,15 @@ Area heap;
 void putch(char ch) {
   putchar(ch);
 }
-
+//intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2);
+//extern intptr_t _syscall_(intptr_t, intptr_t, intptr_t, intptr_t);
+extern int exit(int);
 void halt(int code) {
+  //asm volatile (SYSCALL : "r"(code));
+  exit(code);
+  //_syscall_(0, code, 0, 0);
+  // should not reach here
+  while (1);
   //exit(code);
   //return;
 }
