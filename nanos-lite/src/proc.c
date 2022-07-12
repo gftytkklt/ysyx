c0a768632cd5478632ae09fbd748bc020dfe0f3c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 00:37:48 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:37:48 up  1:22,  1 user,  load average: 0.47, 0.22, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ void init_proc() {
 
   Log("Initializing processes...");
   // load program here
-  naive_uload(NULL,"/bin/nslider");
+  naive_uload(NULL,"/bin/dummy");
 }
 
 Context* schedule(Context *prev) {
