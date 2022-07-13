70a12bb74ef240f17f599b466e6bf71bf2074c92
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 14:59:01 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:59:01 up  1:38,  1 user,  load average: 0.00, 0.05, 0.07

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -52,7 +52,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   cp->mstatus = 0xa00001800;
   cp->mepc = (uintptr_t)entry;
   printf("entry: %p, addr: %p\n", cp->mepc, &cp->mepc);
-  return kstack.start;
+  return *cp1;
   //return NULL;
 }
 
