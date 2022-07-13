faf1ca9beb962e5ee2e117fa495ae3a646a152fb
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 20:52:38 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:52:38 up  1:01,  1 user,  load average: 0.27, 0.22, 0.24

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -46,7 +46,7 @@ Context* schedule(Context *prev) {
   // always select pcb[0] as the new process
   //current = &pcb[0];
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  if(current == &pcb[1]){printf("gpr[10]: %p\n", current->cp->gpr[10]);}
+  printf("gpr[10]: %p\n", current->cp->gpr[10]);
 
   // then return the new context
   return current->cp;
