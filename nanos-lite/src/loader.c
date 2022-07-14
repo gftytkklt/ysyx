4dd0f438c60f9320c1c1960ae628a0c81e29faa5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-14 18:11:39 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:11:39 up 33 min,  1 user,  load average: 0.29, 0.11, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -63,8 +63,8 @@ void context_uload(PCB *pcb, const char *filename){
   //printf("entry = %p\n",entry);
   printf("user kstack: %p, %p\n",kstack.start,kstack.end);
   pcb->cp = ucontext(&pcb->as, kstack, (void*)entry);
-  //((void(*)())entry) ();
-  yield();
+  ((void(*)())entry) ();
+  //yield();
   //printf("cp = %p, cp->%p\n",&pcb->cp,pcb->cp->gpr[10]);
   //return pcb->cp->gpr[10];
 }
