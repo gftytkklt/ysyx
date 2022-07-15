22295b998081fca9a380c2a01b83a2f172786352
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 22:43:08 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:08 up 9 min,  1 user,  load average: 0.09, 0.42, 0.40

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  //printf("in callmain, main = %p\n", (void*)main);
+  printf("in callmain, main = %p\n", (void*)main);
   //int argc = *((int*)args);
   //printf("argc = %d, addr = %p\n", argc, args);
   //char **argv = (char **)((int*)args + 1);
