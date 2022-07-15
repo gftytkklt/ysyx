24e0c7591751e48abed258e763b0663da6c94abe
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 23:11:04 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:11:04 up 37 min,  1 user,  load average: 0.24, 0.16, 0.17

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,17 +1,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   //printf("in callmain, main = %p\n", (void*)main);
   int argc = *((int*)args);
-  //printf("argc = %d, addr = %p\n", argc, args);
+  printf("argc = %d, addr = %p\n", argc, args);
   char **argv = (char **)((int*)args + 1);
-  //printf("argv addr = %p\n", argv);
+  printf("argv addr = %p\n", argv);
+  for (int i=0;i<argc;i++){
+    printf("%s\n", argv[i]);
+  }
   char **envp = (char **)((char **)argv + argc + 1);
-  //printf("envp addr = %p\n", envp);
+  printf("envp addr = %p\n", envp);
   environ = envp;
   //printf("main start\n");
   exit(main(argc, argv, envp));
