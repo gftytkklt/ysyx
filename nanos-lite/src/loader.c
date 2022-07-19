a5e327e6f30a36033d038c428de3040fb1dfd5c7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 19:46:43 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:46:43 up 53 min,  1 user,  load average: 0.29, 0.23, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -79,7 +79,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     envc++;
   }
   //printf("%d %d\n", argc, envc);
-  /*char **argv_stack = (char **)malloc(argc*sizeof(char**));
+  char **argv_stack = (char **)malloc(argc*sizeof(char**));
   char **envp_stack = (char **)malloc(envc*sizeof(char**));
   //printf("test1\n");
   // push envp & argv str to stack
@@ -102,7 +102,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     //printf("%p: %s\n", argv_stack[i-1], argv_stack[i-1]);
   }
   // align with 8 byte
-  stacktop = (void *)((unsigned long)stacktop & 0xfffffffffffffff8);
+  /*stacktop = (void *)((unsigned long)stacktop & 0xfffffffffffffff8);
   // push envc*
   stacktop -= sizeof(char**);
   memset(stacktop, 0, 8);
