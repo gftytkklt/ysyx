0ea572bfaeeb502b75976cfbbb097ca3ea8b3f0e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 19:37:56 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:37:56 up 44 min,  1 user,  load average: 0.16, 0.13, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -122,12 +122,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   stacktop -= sizeof(int);
   *((int*)stacktop) = argc;
   pcb->cp->gpr[10] = (uintptr_t)stacktop;
-  //printf("end: %p\n",pcb->cp->gpr[10]);
-  //asm volatile("ld a0, 0(zero)");
-  //((void(*)())entry) ();
-  //yield();
-  //printf("cp = %p, cp->%p\n",&pcb->cp,pcb->cp->gpr[10]);
-  //return pcb->cp->gpr[10];
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
