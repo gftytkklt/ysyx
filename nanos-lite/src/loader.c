135b19e7f94b90eb2027c4c2971cc42db69aa33f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 11:26:21 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:21 up 26 min,  1 user,  load average: 0.22, 0.21, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -84,6 +84,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   int cplen;
   // push envc str
   for (int i=envc;i>0;i--){
+    printf("%s\n",envp[i-1]);
     cplen = strlen(envp[i-1]);
     stacktop -= (cplen+1);
     strcpy(stacktop,envp[i-1]);
