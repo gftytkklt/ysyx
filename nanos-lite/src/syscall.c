5f8567198b72becdd1ae86bdaaf9981e84767f54
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 11:36:23 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:36:23 up 36 min,  1 user,  load average: 0.22, 0.27, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -123,6 +123,7 @@ int sys_execve(const char *pathname, char *const argv[], char *const envp[]){
     printf("%d: %s\n", i, envp[i]);
     i++;
   }
+  printf("envp num: %d\n",i);
   context_uload(current, pathname, argv, envp);
   switch_boot_pcb();
   yield();
