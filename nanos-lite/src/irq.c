76b611a314dcbfcbfaa6edb2a2e2d83689a68014
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 15:18:21 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:18:21 up 49 min,  1 user,  load average: 0.14, 0.14, 0.11

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,7 @@ void do_syscall(Context *c);
 Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
+  printf("context: %p\n", c);
   switch (e.event) {
     case EVENT_YIELD: printf("yield exec\n");return schedule(c);//printf("yield exec\n");break;
     case EVENT_SYSCALL: do_syscall(c);break;
