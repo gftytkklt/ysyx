5186b49b06ef1ef81a6768edd64a395b57043bef
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 23:57:18 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:57:18 up  8:11,  1 user,  load average: 0.03, 0.07, 0.08

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -3,38 +3,38 @@
 #include <memory/paddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  printf("in mmu trans\n");
-  printf("vaddr=%lx\n",vaddr);
+  //printf("in mmu trans\n");
+  //printf("vaddr=%lx\n",vaddr);
   paddr_t offset = BITS(vaddr,11,0) & 0xfff;
-  printf("%x\n",offset);
+  //printf("%x\n",offset);
   vaddr_t cur_ppn = BITS(cpu.csr[4],43,0);// L2 page nums in satp
-  printf("%lx\n",cur_ppn);
+  //printf("%lx\n",cur_ppn);
   //paddr_t cur_paddr = ppn << 12;// L2 pages addr in satp
   vaddr_t cur_paddr;
   paddr_t shift,cur_vpn;
   for(int level = 2; level >= 0; level--){
-    printf("level=%d\n", level);
+    //printf("level=%d\n", level);
     // current vpn
     shift = level*9 + 12;
-    printf("shift=%d\n",shift);
+    //printf("shift=%d\n",shift);
     cur_vpn = BITS(vaddr,(shift+8),shift) & 0x1ff;
-    printf("cur_vpn=%d\n",cur_vpn);
+    //printf("cur_vpn=%d\n",cur_vpn);
     // paddr = ppn*4096 + vpn*8
     cur_paddr = (cur_ppn<<12) + (cur_vpn<<3);
-    printf("cur_paddr=%lx\n",cur_paddr);
+    //printf("cur_paddr=%lx\n",cur_paddr);
     //vaddr_t *pte = (vaddr_t*)cur_paddr;
     //printf("pte=%p *pte=%lx\n",pte,*pte);
     // update ppn 
     //cur_ppn = BITS((*((paddr_t*)cur_paddr)), 53,10);
     cur_ppn = BITS(paddr_read(cur_paddr,8),53,10);
-    printf("cur_ppn=%lx\n",cur_ppn);
+    //printf("cur_ppn=%lx\n",cur_ppn);
     // exception
     // non-leaf page RWX exception
     // leaf page RWX exception
     // 
     // TODO();
   }
-  printf("paddr = %lx\n",(cur_ppn << 12) + offset);
+  //printf("paddr = %lx\n",(cur_ppn << 12) + offset);
   return (cur_ppn << 12) + offset;
   //return MEM_RET_FAIL;
 }
--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -7,7 +7,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
     return paddr_read(addr, len);
   }
   else{
-    printf("in pte mode ifetch\n");
+    //printf("in pte mode ifetch\n");
     paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
     assert(paddr == addr);
     return paddr_read(paddr, len);
@@ -21,7 +21,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
     return paddr_read(addr, len);
   }
   else{
-    printf("in pte mode read\n");
+    //printf("in pte mode read\n");
     paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
     assert(paddr == addr);
     return paddr_read(paddr, len);
@@ -34,7 +34,7 @@ void vaddr_write(vaddr_t addr, int len, word_t data) {
     paddr_write(addr, len, data);
   }
   else{
-    printf("in pte mode write\n");
+    //printf("in pte mode write\n");
     paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
     assert(paddr == addr);
     paddr_write(paddr, len, data);
