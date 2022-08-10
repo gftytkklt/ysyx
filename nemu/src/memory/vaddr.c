baa989489ed3679febe6a42f221b0840e218a1e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 23:40:31 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:40:31 up  7:54,  1 user,  load average: 0.84, 0.35, 0.14

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -3,7 +3,7 @@
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
   if(isa_mmu_check(vaddr, len, MEM_TYPE_IFETCH) == MMU_DIRECT){
-    printf("in direct mode ifetch\n"); 
+    //printf("in direct mode ifetch\n"); 
     return paddr_read(addr, len);
   }
   else{
@@ -17,7 +17,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 
 word_t vaddr_read(vaddr_t addr, int len) {
   if(isa_mmu_check(vaddr, len, MEM_TYPE_READ) == MMU_DIRECT){
-    printf("in direct mode read\n"); 
+    //printf("in direct mode read\n"); 
     return paddr_read(addr, len);
   }
   else{
@@ -30,7 +30,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
   if(isa_mmu_check(vaddr, len, MEM_TYPE_WRITE) == MMU_DIRECT){
-    printf("in direct mode write\n"); 
+    //printf("in direct mode write\n"); 
     paddr_write(addr, len, data);
   }
   else{
