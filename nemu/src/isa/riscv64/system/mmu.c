0dc06362663ce37b648868703a0ead574bbc2a42
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 23:45:40 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:45:40 up  7:59,  1 user,  load average: 0.05, 0.15, 0.10

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -3,27 +3,38 @@
 #include <memory/paddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
+  printf("in mmu trans\n");
+  printf("%lx\n",vaddr);
   paddr_t offset = BITS(vaddr,11,0) & 0xfff;
+  printf("%x\n",offset);
   vaddr_t cur_ppn = BITS(cpu.csr[4],43,0);// L2 page nums in satp
+  printf("%lx\n",cur_ppn);
   //paddr_t cur_paddr = ppn << 12;// L2 pages addr in satp
   vaddr_t cur_paddr;
   paddr_t shift,cur_vpn;
   for(int level = 2; level > 0; level--){
+    printf("level=%d\n", level);
     // current vpn
     shift = level*9 + 12;
+    printf("%d\n",shift);
     cur_vpn = BITS(vaddr,(shift+8),shift) & 0x1ff;
+    printf("%d\n",cur_vpn);
     // paddr = ppn*4096 + vpn*8
     cur_paddr = (cur_ppn<<12) + (cur_vpn<<3);
+    printf("%lx\n",cur_paddr);
     vaddr_t *pte = (vaddr_t*)cur_paddr;
+    printf("%p\n",pte);
     // update ppn 
     //cur_ppn = BITS((*((paddr_t*)cur_paddr)), 53,10);
     cur_ppn = BITS(*pte,53,10);
+    printf("%lx\n",cur_ppn);
     // exception
     // non-leaf page RWX exception
     // leaf page RWX exception
     // 
     // TODO();
   }
+  printf("%lx\n",(cur_ppn << 12) + offset);
   return (cur_ppn << 12) + offset;
   //return MEM_RET_FAIL;
 }
