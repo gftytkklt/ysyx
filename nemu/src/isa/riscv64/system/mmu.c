969ba20c82a60ab6561c35206337e61d2291a792
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 21:16:36 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:36 up  2:30,  1 user,  load average: 0.02, 0.09, 0.08

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -3,37 +3,36 @@
 #include <memory/paddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  //printf("in mmu trans\n");
+  if(vaddr<0x80000000){printf("in mmu trans\n");}
   paddr_t offset = BITS(vaddr,11,0) & 0xfff;
-  //printf("%x\n",offset);
+  if(vaddr<0x80000000){printf("%x\n",offset);}
   vaddr_t cur_ppn = BITS(cpu.csr[4],43,0);// L2 page nums in satp
-  //printf("%lx\n",cur_ppn);
+  if(vaddr<0x80000000){printf("%lx\n",cur_ppn);}
   //paddr_t cur_paddr = ppn << 12;// L2 pages addr in satp
   vaddr_t cur_paddr;
   paddr_t shift,cur_vpn;
   for(int level = 2; level >= 0; level--){
-    //printf("level=%d\n", level);
+    if(vaddr<0x80000000){printf("level=%d\n", level);}
     // current vpn
     shift = level*9 + 12;
-    //printf("shift=%d\n",shift);
+    if(vaddr<0x80000000){printf("shift=%d\n",shift);}
     cur_vpn = BITS(vaddr,(shift+8),shift) & 0x1ff;
-    //printf("cur_vpn=%d\n",cur_vpn);
+    if(vaddr<0x80000000){printf("cur_vpn=%d\n",cur_vpn);}
     // paddr = ppn*4096 + vpn*8
     cur_paddr = (cur_ppn<<12) + (cur_vpn<<3);
-    //printf("cur_paddr=%lx\n",cur_paddr);
+    if(vaddr<0x80000000){printf("cur_paddr=%lx\n",cur_paddr);}
     //vaddr_t *pte = (vaddr_t*)cur_paddr;
     //printf("pte=%p *pte=%lx\n",pte,*pte);
     // update ppn 
     //cur_ppn = BITS((*((paddr_t*)cur_paddr)), 53,10);
     cur_ppn = BITS(paddr_read(cur_paddr,8),53,10);
-    //printf("cur_ppn=%lx\n",cur_ppn);
+    if(vaddr<0x80000000){printf("cur_ppn=%lx\n",cur_ppn);}
     // exception
     // non-leaf page RWX exception
     // leaf page RWX exception
     // 
     // TODO();
   }
-  if(vaddr < 0x80000000){printf("vaddr=%lx\n",vaddr);printf("paddr = %lx\n",(cur_ppn << 12) + offset);}
   
   return (cur_ppn << 12) + offset;
   //return MEM_RET_FAIL;
