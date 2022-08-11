058e060b335aba7a542077df362402cf9f640ddb
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 21:11:17 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:11:17 up  2:24,  1 user,  load average: 0.18, 0.12, 0.10

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -7,7 +7,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
     return paddr_read(addr, len);
   }
   else{
-    if(addr<0x80000000){printf("in pte mode ifetch, vaddr = %ld\n",addr);}
+    if(addr<0x80000000){printf("in pte mode ifetch, vaddr = %lx\n",addr);}
     paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
     //assert(paddr == addr);
     return paddr_read(paddr, len);
