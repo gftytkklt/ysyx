9c8403ef4be80021c0eeb5656dd9d6f47e4a07ad
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 21:48:36 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:36 up  5:07,  1 user,  load average: 0.02, 0.06, 0.02

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -27,7 +27,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
     //cur_ppn = BITS((*((paddr_t*)cur_paddr)), 53,10);
     if((paddr_read(cur_paddr,8) & 0x1)==0){
       printf("unmapped addr %lx(type%d)!\n",vaddr,type);
-      assert(0);
+      //assert(0);
     }
     cur_ppn = BITS(paddr_read(cur_paddr,8),53,10);
     //if(vaddr<0x80000000){printf("cur_ppn=%lx\n",cur_ppn);}
