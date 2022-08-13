7d29f08a6a6ed4d15303a95df2585f4b1880dfa7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 17:43:31 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:31 up  6:58,  1 user,  load average: 0.35, 0.22, 0.15

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-
+#include <stdio.h>
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
 #endif
@@ -8,5 +8,6 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+  printf("Hello\n");
   return _syscall_(SYS_yield, 0, 0, 0);
 }
