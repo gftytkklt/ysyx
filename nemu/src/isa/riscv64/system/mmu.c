556267a4adce2b1f0010f050e4a6f829968ffbd1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 15:21:53 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:53 up  4:36,  1 user,  load average: 0.25, 0.22, 0.14

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -5,24 +5,24 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   if(vaddr<0x80000000){printf("in mmu trans\n");}
   paddr_t offset = BITS(vaddr,11,0) & 0xfff;
-  //if(vaddr<0x80000000){printf("%x\n",offset);}
+  if(vaddr<0x80000000){printf("%x\n",offset);}
   vaddr_t cur_ppn = BITS(cpu.csr[4],43,0);// L2 page nums in satp
-  //if(vaddr<0x80000000){printf("%lx\n",cur_ppn);}
+  if(vaddr<0x80000000){printf("%lx\n",cur_ppn);}
   //paddr_t cur_paddr = ppn << 12;// L2 pages addr in satp
   vaddr_t cur_paddr;
   paddr_t shift,cur_vpn;
   for(int level = 2; level >= 0; level--){
-    //if(vaddr<0x80000000){printf("level=%d\n", level);}
+    if(vaddr<0x80000000){printf("level=%d\n", level);}
     // current vpn
     shift = level*9 + 12;
-    //if(vaddr<0x80000000){printf("shift=%d\n",shift);}
+    if(vaddr<0x80000000){printf("shift=%d\n",shift);}
     cur_vpn = BITS(vaddr,(shift+8),shift) & 0x1ff;
-    //if(vaddr<0x80000000){printf("cur_vpn=%d\n",cur_vpn);}
+    if(vaddr<0x80000000){printf("cur_vpn=%d\n",cur_vpn);}
     // paddr = ppn*4096 + vpn*8
     cur_paddr = (cur_ppn<<12) + (cur_vpn<<3);
-    //if(vaddr<0x80000000){printf("cur_paddr=%lx\n",cur_paddr);}
-    //vaddr_t *pte = (vaddr_t*)cur_paddr;
-    //printf("pte=%p *pte=%lx\n",pte,*pte);
+    if(vaddr<0x80000000){printf("cur_paddr=%lx\n",cur_paddr);}
+    vaddr_t *pte = (vaddr_t*)cur_paddr;
+    printf("pte=%p *pte=%lx\n",pte,*pte);
     // update ppn 
     //cur_ppn = BITS((*((paddr_t*)cur_paddr)), 53,10);
     uint64_t cur_pte = paddr_read(cur_paddr,8);
