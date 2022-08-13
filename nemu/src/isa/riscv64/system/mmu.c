be0386747e063e70e404da7a9dca93b7efdecb7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 17:29:54 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:29:54 up  6:44,  1 user,  load average: 0.12, 0.13, 0.07

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -31,10 +31,10 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
       printf("addr %lx, cur_paddr %lx, pte %lx(level%d, type%d)!\n",vaddr,cur_paddr,cur_pte,level,type);
     }*/
     cur_ppn = BITS(cur_pte,53,10);
-    /*if((cur_pte & 0x1)==0){
+    if((cur_pte & 0x1)==0){
       printf("unmapped addr %lx, invalid cur_paddr %lx,satp = %lx, pte %lx, next_paddr = %lx(level%d, type%d)!\n",vaddr,cur_paddr,cpu.csr[4],cur_pte,(cur_ppn << 12),level,type);
-      assert(0);
-    }*/
+      //assert(0);
+    }
     // if(vaddr<0x80000000){printf("cur_ppn=%lx\n",cur_ppn);}
     // exception
     // non-leaf page RWX exception
