d783ebe3e33f70311cf67f1c112558adc2bd67b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-15 22:03:00 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:03:00 up  2:58,  1 user,  load average: 0.39, 0.18, 0.11

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -22,7 +22,7 @@ word_t isa_query_intr() {
   //printf("INTR: %d, matatus: %lx\n",cpu.INTR, cpu.csr[1]);
   if(cpu.INTR && (cpu.csr[1] & 0x8)){
   //if(cpu.INTR){
-    //printf("timer irq\n");
+    printf("timer irq\n");
     cpu.INTR = 0;
     return IRQ_TIMER;
   }
