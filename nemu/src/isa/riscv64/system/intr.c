f4f8e0193ef09bbf5ded6e0d763f452ef1e6ae9d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-15 18:09:00 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:09:00 up 52 min,  1 user,  load average: 0.01, 0.14, 0.17

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -6,10 +6,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   word_t mie = BITS(cpu.csr[1],3,3);
   cpu.csr[0] = epc;// mepc = epc
-  //printf("mstatus before: %lx\n",cpu.csr[1]);
+  printf("mstatus before: %lx\n",cpu.csr[1]);
   cpu.csr[1] = (cpu.csr[1] & (~0x88ul)) | (mie << 7);
   //cpu.csr[1] = 
-  //printf("mstatus after: %lx\n",cpu.csr[1]);
+  printf("mstatus after: %lx\n",cpu.csr[1]);
   cpu.csr[2] = NO;// mcause = NO
   return cpu.csr[3];// dnpc = mtvec
   //assert((BITS(cpu.csr[3],1,0) == 2) || (BITS(cpu.csr[3],1,0) == 0);
@@ -22,7 +22,7 @@ word_t isa_query_intr() {
   //printf("INTR: %d, matatus: %lx\n",cpu.INTR, cpu.csr[1]);
   if(cpu.INTR && (cpu.csr[1] & 0x8)){
   //if(cpu.INTR){
-    printf("timer irq\n");
+    //printf("timer irq\n");
     cpu.INTR = 0;
     return IRQ_TIMER;
   }
