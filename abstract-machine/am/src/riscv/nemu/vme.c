5767d15b13dd0b2f03801a28582bdbaa957c3255
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 09:00:59 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:00:58 up 17 min,  1 user,  load average: 0.16, 0.17, 0.26

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -15,14 +15,14 @@ static Area segments[] = {      // Kernel memory mappings
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
-  printf("set satp\n");
+  printf("set satp at %p\n",pdir);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
 static inline uintptr_t get_satp() {
   uintptr_t satp;
-  printf("get satp\n");
   asm volatile("csrr %0, satp" : "=r"(satp));
+  printf("get satp at %p\n",satp << 12);
   return satp << 12;
 }
 
