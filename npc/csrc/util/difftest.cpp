6ad5c255362500c6646a7473ed5941ea3c3c58f3
gftytkklt <mf20230171@smail.nju.edu.cn>
2022-11-07 14:29:00 +0800

    forward pipeline with difftest bug fixed

--- npc/csrc/util/difftest.cpp
+++ npc/csrc/util/difftest.cpp
@@ -14,6 +14,7 @@ const char *regs[] = {
 };
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 static bool is_skip_ref = false;
+static uint64_t skip_pc = 0;
 static uint64_t cpu_context[33];
 void (*nemu_difftest_memcpy)(uint64_t addr, void* buf, size_t n, bool direction) = NULL;
 void (*nemu_difftest_regcpy)(void* dut, bool direction) = NULL;
@@ -46,22 +47,24 @@ void init_difftest(char *ref_so_file, long img_size, uint8_t* mem, uint64_t *cpu
 	//printf("test2\n");
 }
 
-void difftest_skip_ref() {
+void difftest_skip_ref(uint64_t pc) {
 	is_skip_ref = true;
+	skip_pc = pc;
 }
 
-void difftest_step(uint64_t pc, uint64_t* dut, uint64_t sim_time){
+void difftest_step(uint64_t pc, uint64_t* dut, uint64_t sim_time, bool* error){
 	//for(int i=0;i<32;i++){dut_context.gpr[i] = dut[i];}
 	//dut_context.pc = pc;
 	uint64_t ref_data[32];
-	if (is_skip_ref) {
+	//printf("exec nemu at pc = %lx\n",pc);
+	if (is_skip_ref && (skip_pc == pc)) {
         	// to skip the checking of an instruction, just copy the reg state to reference design
-        	//printf("skip ref\n");
+        	//printf("skip ref at pc = %lx\n", pc);
         	//nemu_difftest_regcpy(dut, DIFFTEST_TO_REF);
         	for(int i = 0;i<32;i++){
 	  		cpu_context[i] = dut[i];
 		}
-		cpu_context[32] = pc;
+		cpu_context[32] = pc+4;
         	nemu_difftest_regcpy(cpu_context, DIFFTEST_TO_REF);
         	is_skip_ref = false;
         	return;
@@ -70,6 +73,6 @@ void difftest_step(uint64_t pc, uint64_t* dut, uint64_t sim_time){
 	//printf("ref exec\n");
 	nemu_difftest_regcpy(ref_data, DIFFTEST_TO_DUT);
 	for(int i=0;i<32;i++){
-		if(dut[i] != ref_data[i]){printf("time: %ld, pc: %lx, reg %s does not match! ref: %lx, dut: %lx\n", sim_time, pc, regs[i], ref_data[i], dut[i]);}
+		if(dut[i] != ref_data[i]){printf("time: %ld, pc: %lx, reg %s does not match! ref: %lx, dut: %lx\n", sim_time, pc, regs[i], ref_data[i], dut[i]);*error = true;}
 	}
 }
