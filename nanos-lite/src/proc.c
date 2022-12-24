cad088ae25eedfec5c29e53c595b7860eee31e62
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-24 23:45:21 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:45:21 up  2:03,  1 user,  load average: 0.12, 0.17, 0.12

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -55,18 +55,18 @@ void init_proc() {
   char * envp[]={NULL};
   
   // 1224 zhushi
-  //context_uload(&pcb[0],"/bin/hello", argv, envp);
-  //context_uload(&pcb[1],"/bin/pal", argv, envp);
-  //context_uload(&pcb[2],"/bin/nslider", argv, envp);
-  //1224 add
-  context_kload(&pcb[0],hello_fun,"arg0");
-  //context_kload(&pcb[1],hello_fun,"arg1");
+  context_uload(&pcb[0],"/bin/hello", argv, envp);
   context_uload(&pcb[1],"/bin/pal", argv, envp);
+  context_uload(&pcb[2],"/bin/nslider", argv, envp);
+  //1224 add
+  //context_kload(&pcb[0],hello_fun,"arg0");
+  //context_kload(&pcb[1],hello_fun,"arg1");
+  //context_uload(&pcb[1],"/bin/pal", argv, envp);
   
-  fg_pcb = &pcb[0];
+  fg_pcb = &pcb[1];
   //Log("proc end at %lx\n",pcb[0].max_brk);
 }
-//static int i = 0;
+static int i = 0;
 Context* schedule(Context *prev) {
   // save the context pointer
   //printf("schedule begin, cp: %p\n",prev);
@@ -82,10 +82,10 @@ Context* schedule(Context *prev) {
   //current = (i%100) ? &pcb[1] : &pcb[0];
   
   //1224 zhushi
-  //i++;
-  //current = (i%100) ? fg_pcb : &pcb[0];
+  i++;
+  current = (i%100) ? fg_pcb : &pcb[0];
   //1224 add
-  current = (current == &pcb[1]) ? &pcb[0] : &pcb[1];
+  //current = (current == &pcb[1]) ? &pcb[0] : &pcb[1];
   
   //if(current == &pcb[0]){printf("after: current pcb: pcb0\n");}
   //else if(current == &pcb[1]){printf("after: current pcb: pcb1\n");}
