4333dcbf7d2b49e91ecabddd46e7f070a34c0509
gftytkklt <mf20230171@smail.nju.edu.cn>
2023-01-04 16:33:04 +0800

    create csrfiles

--- npc/vsrc_fullpipline/ysyx_22040750_csr.v
+++ npc/vsrc_fullpipline/ysyx_22040750_csr.v
@@ -8,10 +8,40 @@ module ysyx_22040750_csr(
     input [63:0] I_wr_data,
     output [63:0] O_rd_data
 );
+    localparam CSR_NUM = 1 << 12;
     localparam MEPC = 12'h341;
     localparam MSTATUS = 12'h300;
     localparam MTVEC = 12'h305;
     localparam MCAUSE = 12'h342;
     reg [63:0] mepc, mstatus, mtvec, mcause;
-    reg [63:0] mip, mie, mtime, mtimecmp;
+    reg [63:0] rd_data;
+    assign O_rd_data = rd_data;
+    //reg [63:0] mip, mie, mtime, mtimecmp; clint as mmio p
+    always @(posedge I_sys_clk)
+        if(I_rst) begin
+            {mepc, mtvec, mcause} <= 'h0;
+            mstatus <= 'ha00001800;
+        end
+        else if(I_csr_wen) 
+            case(I_wr_addr)
+                `MEPC: mepc <= I_wr_data;
+                `MSTATUS: mstatus <= I_wr_data;
+                `MTVEC: mtvec <= I_wr_data;
+                `MCAUSE: mcause <= I_wr_data;
+                default:;
+            endcase
+        else begin
+            mepc <= mepc;
+            mstatus <= mstatus;
+            mtvec <= mtvec;
+            mcause <= mcause;
+        end
+    always @(*)
+        case(I_rd_addr)
+            `MEPC: rd_data = mepc;
+            `MSTATUS: rd_data = mstatus;
+            `MTVEC: rd_data = mtvec;
+            `MCAUSE: rd_data = mcause;
+            default: rd_data = 'h0;
+        endcase
 endmodule
