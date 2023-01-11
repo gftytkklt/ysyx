2e94c5b10a0db35f9d35ae8a059a9e596b02f7f9
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-11 20:42:51 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:51 up  3:45,  1 user,  load average: 0.09, 0.10, 0.09

--- npc/csrc/cputest.cpp
+++ npc/csrc/cputest.cpp
@@ -13,9 +13,9 @@
 #include "Vysyx_22040750_cpu_top__Dpi.h"
 //#define N 32
 //#define CONFIG_FTRACE
-#define CONFIG_ITRACE
+//#define CONFIG_ITRACE
 #define CONFIG_DIFFTEST
-//#define CONFIG_WAVEFORM
+#define CONFIG_WAVEFORM
 #define ASNI_FG_RED     "\33[1;31m"
 #define ASNI_FG_GREEN   "\33[1;32m"
 #define ASNI_NONE       "\33[0m"
