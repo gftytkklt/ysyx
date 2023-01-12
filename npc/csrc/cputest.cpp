a2875192560e0e4e2c41f7cc612fa1736ac8fecb
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-12 18:31:05 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:31:04 up  5:40,  1 user,  load average: 0.24, 0.14, 0.10

--- npc/csrc/cputest.cpp
+++ npc/csrc/cputest.cpp
@@ -16,7 +16,7 @@
 //#define CONFIG_MTRACE
 #define CONFIG_ITRACE
 #define CONFIG_DIFFTEST
-//#define CONFIG_WAVEFORM
+#define CONFIG_WAVEFORM
 #define ASNI_FG_RED     "\33[1;31m"
 #define ASNI_FG_GREEN   "\33[1;32m"
 #define ASNI_NONE       "\33[0m"
