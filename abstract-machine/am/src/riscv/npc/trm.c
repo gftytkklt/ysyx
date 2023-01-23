dd05572066dd036627ea202631e0b9f850fc3889
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-23 12:01:21 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:01:21 up  1:06,  1 user,  load average: 0.20, 0.16, 0.18

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -20,7 +20,7 @@ void putch(char ch) {
 }
 
 void halt(int code) {
-  printf("return code = %x\n", code);
+  printf("return code = %02x\n", code);
   asm volatile("mv a0, %0; ebreak" : :"r"(code));
   while (1);
 }
