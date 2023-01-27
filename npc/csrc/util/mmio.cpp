706269d4735804e9b8816d2b7d49bfb960c32705
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 17:38:28 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:38:28 up  5:10,  1 user,  load average: 0.54, 0.31, 0.19

--- npc/csrc/util/mmio.cpp
+++ npc/csrc/util/mmio.cpp
@@ -15,10 +15,10 @@ void get_keybrd(unsigned long* keycode){
 }
 
 void update_screen(screeninfo* drawinfo){
-    // int x = drawinfo->x;
-    // int y = drawinfo->y;
-    // int w = drawinfo->w;
-    // int h = drawinfo->h;
-    // void* pixels = drawinfo->pixels;
-    // printf("draw info: (%d %d %d %d), pixels at %p\n",x,y,w,h,pixels);
+    int x = drawinfo->x;
+    int y = drawinfo->y;
+    int w = drawinfo->w;
+    int h = drawinfo->h;
+    void* pixels = drawinfo->pixels;
+    printf("draw info: (%d %d %d %d), pixels at %p\n",x,y,w,h,pixels);
 }
