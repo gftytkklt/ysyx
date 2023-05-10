0ffad4187a2cf50fa6f67e1bfce8b36f4b73ea1a
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 15:57:53 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:57:53 up  1:05,  1 user,  load average: 0.27, 0.21, 0.16

--- npc/csrc/util/readelf.cpp
+++ npc/csrc/util/readelf.cpp
@@ -12,6 +12,7 @@ typedef struct elf_func{
   char func_name[BUF_SIZE];
   unsigned long entry_addr;
   unsigned long func_size;
+  unsigned int call_depth;
 }func;
 static func func_pool[FUNC_NUM] = {};
 
@@ -105,6 +106,7 @@ void init_elf(char* elf_file) {
     buf_assignment(elf_fp, (symofft+i*symentsize+STINFO_OFFT), SEEK_SET, buf, sizeof(unsigned char));
     symtype = *((unsigned char*) buf);
     if (symtype == FUNC) {
+      func_pool[func_idx].call_depth = 0;
       // get func entry addr
       buf_assignment(elf_fp, (symofft+i*symentsize+STADDR_OFFT), SEEK_SET, buf, sizeof(unsigned long));
       func_pool[func_idx].entry_addr = *((unsigned long*) buf);
@@ -138,13 +140,15 @@ void print_ftrace(unsigned long time, unsigned long pc, unsigned inst, FILE* fp)
       func_depth++;
       //fprintf(fp, "%lx:%*s",pc,func_depth," ");
       // print info & depth update
+      func_pool[i].call_depth = func_depth;
       fprintf(fp, "[lvl%d]%lx: call [%s@0x%lx](%lu)\n",func_depth, pc, func_pool[i].func_name,func_pool[i].entry_addr, time);
     }
     //else if((dnpc > func_pool[i].entry_addr) && (dnpc < func_pool[i].entry_addr + func_pool[i].func_size) && (inst==0x00008067)){
     else if((pc >= func_pool[i].entry_addr) && (pc < func_pool[i].entry_addr + func_pool[i].func_size) && (inst==0x00008067)){
       //fprintf(fp, "%lx:%*s",pc,func_depth," ");
       fprintf(fp, "[lvl%d]%lx: ret [%s](%lu)\n",func_depth, pc, func_pool[i].func_name, time);
-      func_depth--;
+      func_depth = func_pool[i].call_depth;
+      //func_depth--;
     }
     else if(inst == 0x00100073){
       fprintf(fp, "%lx:%*s[ebreak](%lu)\n",pc,func_depth," ", time);
