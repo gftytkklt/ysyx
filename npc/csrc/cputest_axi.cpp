b3c17ae8142974b2fe2a6dd65563da68c50599d6
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 16:36:35 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:35 up  1:33,  1 user,  load average: 0.74, 0.35, 0.20

--- npc/csrc/cputest_axi.cpp
+++ npc/csrc/cputest_axi.cpp
@@ -17,7 +17,7 @@ static long img_size = 0;
 extern const char* regs[];
 uint8_t* mem=NULL;
 vluint64_t sim_time = 0;
-uint64_t dump_inst = 197000;
+uint64_t dump_inst = 0;
 uint32_t dump_pc = 0x0;
 bool dump_en = false;
 
