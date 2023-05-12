6a152551e5234dcc67a422c4eaad013748bb3882
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 18:25:56 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:25:56 up  3:23,  1 user,  load average: 0.53, 0.48, 0.44

--- npc/build/obj_dir/Vysyx_22040750___024root__2.cpp
+++ npc/build/obj_dir/Vysyx_22040750___024root__2.cpp
@@ -5037,7 +5037,9 @@ VL_INLINE_OPT void Vysyx_22040750___024root___combo__TOP__7(Vysyx_22040750___024
                                                     & (IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__IF_ID_allowin))) 
                                                 & (IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__pc_e__DOT__IF_en));
     vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__npc_e__DOT__store_dnpc 
-        = ((IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid) 
+        = ((((IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__IF_ID_valid) 
+             & (~ (IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__IF_ID_bubble))) 
+            & (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid)) 
            & (~ (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_ready)));
     vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__pc_e__DOT__IF_handshake 
         = ((IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid) 
