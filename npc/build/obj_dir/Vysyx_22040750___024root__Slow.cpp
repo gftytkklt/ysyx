e2c707278b1c6b0e98d5f9a47b27f5989764c500
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-12 18:15:14 +0800

    >  sim RTL ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:15:14 up  3:12,  1 user,  load average: 0.15, 0.13, 0.15

--- npc/build/obj_dir/Vysyx_22040750___024root__Slow.cpp
+++ npc/build/obj_dir/Vysyx_22040750___024root__Slow.cpp
@@ -2314,11 +2314,8 @@ void Vysyx_22040750___024root___settle__TOP__1(Vysyx_22040750___024root* vlSelf)
                                             ? 1U : (IData)(vlSelf->ysyx_22040750__DOT__cache_e__DOT__dcache_e__DOT__current_state))
                                         : (IData)(vlSelf->ysyx_22040750__DOT__cache_e__DOT__dcache_e__DOT__current_state)))))));
     vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__npc_e__DOT__store_dnpc 
-        = (((IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__IF_ID_valid) 
-            & ((~ (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid)) 
-               | (~ (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_ready)))) 
-           | ((IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__npc_e__DOT__dnpc_reg_valid) 
-              & (IData)(vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__IF_ID_valid)));
+        = ((IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid) 
+           & (~ (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_ready)));
     vlSelf->ysyx_22040750__DOT__cpu_core_e__DOT__pc_e__DOT__IF_handshake 
         = ((IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_valid) 
            & (IData)(vlSelf->ysyx_22040750__DOT__cpu_pc_ready));
