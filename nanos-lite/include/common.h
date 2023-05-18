de4b26aa7e27004ddb10bd0fae3c3249125a659e
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-18 15:56:42 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:42 up 37 min,  1 user,  load average: 0.08, 0.04, 0.11

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -3,8 +3,11 @@
 
 /* Uncomment these macros to enable corresponding functionality. */
 #define HAS_CTE
-#define HAS_VME
-//#define TEST_NPC
+//#define HAS_VME
+#define TEST_NPC
+#ifndef TEST_NPC
+  #define HAS_VME
+#endif
 //#define MULTIPROGRAM
 //#define TIME_SHARING
 
