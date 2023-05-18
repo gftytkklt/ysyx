dd445b40f160be6c04d7714ab6bccf349840afb1
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-18 16:05:54 +0800

    >  compile NEMU ysyx_22040750 朱子谦 Linux gftyt-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:54 up 46 min,  1 user,  load average: 0.23, 0.26, 0.23

--- abstract-machine/scripts/riscv64-nemu.mk
+++ abstract-machine/scripts/riscv64-nemu.mk
@@ -1,8 +1,17 @@
 include $(AM_HOME)/scripts/isa/riscv64.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-
-AM_SRCS += riscv/nemu/start.S \
+ifeq ($(TEST_NPC),)
+	AM_SRCS += riscv/npc/start.S \
+		   riscv/npc/cte.c \
+		   riscv/npc/trap.S
+else
+	AM_SRCS += riscv/nemu/start.S \
+		   riscv/nemu/cte.c \
+		   riscv/nemu/trap.S \
+		   riscv/nemu/vme.c
+endif
+#AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
            riscv/nemu/trap.S \
            riscv/nemu/vme.c
